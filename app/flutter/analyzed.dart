1a135d356e427ae149d754b0525dd7f3f8273065
CHOPHILSUN <fohsdpbls62371@gmail.com>
2024-01-26 16:12:34 +0900

    upload 2024-01-26 files to remote sites

--- app/flutter/analyzed.dart
+++ app/flutter/analyzed.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '아기울음 분석',
-      home: ColicPage(),
-    );
-  }
-}
 
 
-class ColicPage extends StatelessWidget {
+class analyzed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,20 +13,20 @@ class ColicPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Image.asset('images/analyzing.png'), // 이미지 경로를 실제 이미지 파일로 변경하세요.
             Text(
-              "복통이 있는 것 같아요",
+              "분석중",
               style: TextStyle(fontSize: 42),
             ),
             SizedBox(height: 20),
-            Image.asset('images/Dapino-Baby-Boy-Baby-crying 2.png'), // 이미지 경로를 실제 이미지 파일로 변경하세요.
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // 홈으로 이동하는 기능을 구현하세요.
-                // 예를 들면 Navigator를 사용하여 홈 화면으로 이동할 수 있습니다.
-              },
-              child: Text("홈으로", style: TextStyle(fontSize: 42)),
-            ),
+            /// ElevatedButton(
+            ///  onPressed: () {
+            // 홈으로 이동하는 기능을 구현하세요.
+            // 예를 들면 Navigator를 사용하여 홈 화면으로 이동할 수 있습니다.
+            ///  },
+            ///  child: Text("홈으로", style: TextStyle(fontSize: 42)),
+            /// ),
           ],
         ),
       ),
